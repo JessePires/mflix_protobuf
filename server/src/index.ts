f7010e33c99eb253c65a222a9e0c51ea218d25bc
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2023-05-06 17:47:26 -0300

    refactor: modularizando funcoes usadas no codigo

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,11 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-import { Movie, Genre, Cast, Language, Director, Writer, Country } from './generated/src/proto/movies_pb'
-import { Document, MongoClient, ObjectId, ServerApiVersion, WithId } from 'mongodb';
+import { Movie} from './generated/src/proto/movies_pb'
+import {  MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
 import { Collection } from 'mongodb';
-import { log } from 'console';
+import { createMovieProtobuf } from './utils/createMovieProtobuf';
+import { createFakeMovieInBytes } from './utils/createFakeMovieInBytes';
 
 const uri = process.env.MONGO_URI || '';
 const database = process.env.DB_NAME || "sample_mflix";
@@ -17,71 +18,6 @@ const client = new MongoClient(uri,  {
     deprecationErrors: true,
   }
 });
-
-function createMovieProtobuf(movie: WithId<Document>): Movie{
-  try {
-    const protoMovie = new Movie();
-
-    const protoGenres = movie.genres?.map((value: string) => {
-      const genre = new Genre();
-      genre.setName(value);
-      return genre;
-    })
-
-    const protoCasts = movie.cast?.map((value: string) => {
-      const cast = new Cast();
-      cast.setActor(value);
-      return cast;
-    })
-
-    const protoLanguages = movie.languages?.map((value: string) => {
-      const language = new Language();
-      language.setName(value);
-      return language;
-    })
-
-    const protoDirectors = movie.directors?.map((value: string) => {
-      const director = new Director();
-      director.setName(value);
-      return director;
-    })
-
-    const protoWriters = movie.writers?.map((value: string) => {
-      const writer = new Writer();
-      writer.setName(value);
-      return writer;
-    })
-
-    const protoCountries = movie.countries?.map((value: string) => {
-      const contry = new Country();
-      contry.setName(value);
-      return contry;
-    })
-
-    protoMovie.setId(String(movie._id));
-    if(movie?.plot) protoMovie.setPlot(movie.plot);
-    if(protoGenres) protoMovie.setGenresList(protoGenres);
-    if(movie?.runtime) protoMovie.setRuntime(movie.runtime);
-    if(protoCasts) protoMovie.setCastList(protoCasts);
-    if(movie?.numMFlixComments) protoMovie.setNumMflixComments(movie.numMFlixComments);
-    if(movie?.poster) protoMovie.setPoster(movie.poster);
-    if(movie?.title) protoMovie.setTitle(movie.title);
-    if(movie?.fullplot) protoMovie.setFullplot(movie.fullplot);
-    if(protoLanguages) protoMovie.setLanguagesList(protoLanguages);
-    if(movie?.released) protoMovie.setReleased(movie.released);
-    if(protoDirectors) protoMovie.setDirectorsList(protoDirectors);
-    if(protoWriters) protoMovie.setWritersList(protoWriters);
-    if(movie?.lastupdated) protoMovie.setLastupdated(movie.lastupdated);
-    if(movie?.year) protoMovie.setYear(movie.year);
-    if(protoCountries) protoMovie.setCountriesList(protoCountries);
-    if(movie?.type) protoMovie.setType(movie.type);
-
-
-    return protoMovie;
-  } catch (error) {
-    throw error;
-  }
-}
 
 async function getMovieById(collections: Collection, id: string){
   try {
@@ -124,7 +60,6 @@ async function getAllMovies(collection: Collection){
   }
 }
 
-
 async function createMovie(collection: Collection, movie: Movie): Promise<string | null>{
   try {
 
@@ -159,39 +94,6 @@ async function createMovie(collection: Collection, movie: Movie): Promise<string
   }
 }
 
-function createFakeMovieInBytes(): Uint8Array{
-  const fakeProtoMovie = new Movie();
-
-  const genre = new Genre();
-  genre.setName("genre 1")
-
-  const cast = new Cast();
-  cast.setActor("jhonatan");
-
-  const country = new Country();
-  country.setName("country")
-
-  const director = new Director();
-  director.setName("diretor jhonatan")
-
-  fakeProtoMovie.setPlot("plot");
-  fakeProtoMovie.setGenresList([genre]);
-  fakeProtoMovie.setRuntime(1);
-  fakeProtoMovie.setCastList([cast]);
-  fakeProtoMovie.setNumMflixComments(20);
-  fakeProtoMovie.setTitle("Titulo");
-  fakeProtoMovie.setFullplot("fullplot");
-  fakeProtoMovie.setCountriesList([country]);
-  fakeProtoMovie.setReleased("1893-05-09T00:00:00.000Z");
-  fakeProtoMovie.setDirectorsList([director]);
-  fakeProtoMovie.setRated("UNRATED");
-  fakeProtoMovie.setLastupdated("2015-08-26 00:03:50.133000000");
-  fakeProtoMovie.setYear("2023");
-  fakeProtoMovie.setType("movie");
-
-
-  return fakeProtoMovie.serializeBinary();
-}
 
 async function run() {
   try {
